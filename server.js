209632022e5645496d0ae1a87c78e60933bd6a25
McleodHerritt <nmcleod2021@gmail.com>
2023-11-18 16:20:56 -0400

    set up handlerbars folder structure and render a test homepage

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-const routes = require("./controller/api");
+const routes = require("./controller");
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
